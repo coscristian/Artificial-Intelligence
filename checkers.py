03cc06e02437800bc004c3b9d3f8afca2c8d1c66
Crristian Quesada Cossio <crisquesadaco@gmail.com>
2023-03-23 15:21:41 -0500

    finished legal movements

--- checkers.py
+++ checkers.py
@@ -110,18 +110,18 @@ def get_enemy_board_pieces(board, enemy_turn):
   for i in range(len(board)):
     for j in range(len(board[i])):
         if not enemy_turn:   
-          if board[i][j] > 2:
+          if board[i][j] > 2 and board[i][j] != 0:
             board_pieces.append(board[i][j])
-        elif board[i][j] <= 2:
+        elif board[i][j] <= 2 and board[i][j] != 0:
             board_pieces.append(board[i][j])
   return board_pieces
 
 def get_piece_position(board, piece):
-   for i in range(len(board)):
-      for j in range(len(board[i])):
-         if board[i][j] == piece:
-            return (i, j)
-
+  for i in range(len(board)):
+    for j in range(len(board[i])):
+        if board[i][j] == piece:
+          return (i, j)
+  return (None, None)
 def can_only_eat_one_piece(board, x, y):
   if board[x - 1][y - 1] != 0 or board[x-1][y + 1] != 0:
     return True
@@ -136,20 +136,20 @@ def can_only_eat_one_piece(board, x, y):
 """
 def can_eat(board, x, y, enemy_turn):
   enemy_pieces = get_enemy_board_pieces(board, enemy_turn)
-  enemy_pieces.sort()
+  #enemy_pieces.sort()
   for enemy_piece in enemy_pieces:
-    if enemy_piece == board[x - 1][y - 1]:
+    if x > 0 and y > 0 and enemy_piece == board[x - 1][y - 1] and board[x-1][y+1] != 0:
       return [x - 1, y - 1]
-    elif enemy_piece == board[x - 1][y + 1]:
+    elif x > 0 and y < len(board[x]) - 1 and enemy_piece == board[x - 1][y + 1]:
       return [x - 1, y + 1]
   return (None, None)
 
 def eat_piece(board, x_to_eat, y_to_eat):
-  print(x_to_eat)
+  print("Piece to eat: ", board[x_to_eat][y_to_eat])
   board[x_to_eat][y_to_eat] = 0
 
 def move_to_new_position(board, x_to_move, y_to_move):
-  board[x_to_move][y_to_move] = 3 #Change this piece, it must be dinamically
+  board[x_to_move][y_to_move] = 5 #Change this piece, it must be dinamically
 
 def delete_current_position(board, x, y):
   board[x][y] = 0
@@ -164,6 +164,7 @@ def can_eat_both_pieces(board, x, y, enemy_turn):
 
 def check_diagonal_moves(x, y, board, enemy_turn):
   moves = []
+  flag_eat = False
   if x > 0: #Can move up
     if y == 0: # Check only right diagonal
       legal_move = check_move_right_diagonal(x, y, board, enemy_turn)
@@ -183,10 +184,16 @@ def check_diagonal_moves(x, y, board, enemy_turn):
 
     number_legal_moves = len(moves)
     if number_legal_moves == 1:
-      x_to_eat, y_to_eat = can_eat(board, x, y, enemy_turn)
-      x_to_move = moves[0][0]
-      y_to_move = moves[0][1]
-      if x_to_eat is not None:
+      x_legal_move = moves[0][0]
+      y_legal_move = moves[0][1]
+      if board[x_legal_move][y_legal_move] != 0:
+        x_to_eat, y_to_eat = can_eat(board, x, y, enemy_turn)
+        flag_eat = True
+
+      x_to_move = x_legal_move
+      y_to_move = y_legal_move
+      
+      if flag_eat:
         eat_piece(board, x_to_eat, y_to_eat)
         #move_to_new_position(board, x_to_eat - 1, y_to_eat - 1)
       move_to_new_position(board, x_to_move, y_to_move)
@@ -196,15 +203,16 @@ def check_diagonal_moves(x, y, board, enemy_turn):
          #if not enemy_turn:
         x_to_eat, y_to_eat = get_piece_position(board, min(get_enemy_board_pieces(board, enemy_turn)))
         eat_piece(board, x_to_eat, y_to_eat)
+        print("x: ", x, " y: ", y)
         move_to_new_position(board, x_to_eat - 1, y_to_eat - 1)
         delete_current_position(board, x, y)
          #else:
         #    x_to_eat, y_to_eat = get_piece_position(board, min(get_enemy_board_pieces(board, enemy_turn)))
         #    get_piece_position()
       elif can_only_eat_one_piece(board, x, y): # There is a empty position and a busy position
-        x_to_eat, y_to_eat = x - 1, y - 1 if board[x-1][y+1] == 0 else x - 1, y + 1
+        x_to_eat, y_to_eat = (x - 1, y - 1) if board[x-1][y+1] == 0 else (x - 1, y + 1)
         #if position_has_enemy(moves, enemy_turn):
-        eat_piece(x_to_eat, y_to_eat)
+        eat_piece(board, x_to_eat, y_to_eat)
         move_to_new_position(board, x_to_eat - 1, y_to_eat - 1)
         delete_current_position(board, x, y)
       else:
@@ -232,19 +240,35 @@ def convert_moves(moves):
         converted_moves.append((3 - pair[0], 3 - pair[1]))
     return converted_moves
 
-def generate_legal_moves(player, board):
+def select_coords_piece(board, enemy_turn):
+  first_option = 1
+  second_option = 2
+  if enemy_turn:
+    first_option = 3
+    second_option = 4  
+
+  x, y = get_piece_position(board, first_option) 
+  if x == None:
+    x, y = get_piece_position(board, second_option)
+  return x, y
+
+def generate_legal_moves(board, player):
   enemy_turn = False
   legal_moves = []
-  x = player[0]
-  y = player[1]
-
+  
+  if player == "p2":
+    enemy_turn = True
+  
+  x, y = select_coords_piece(board, enemy_turn)
+  
   #try:
   if enemy_turn:
       board = inverse_board(board[:])
       legal_moves = check_diagonal_moves(3 - x, 3 - y, board, enemy_turn)
       legal_moves = convert_moves(legal_moves)
+      board = inverse_board(board[:])
   else:
-      legal_moves = check_diagonal_moves(x, y, board, False)
+      legal_moves = check_diagonal_moves(x, y, board, enemy_turn)
   print("Legal moves")
   for move in legal_moves:
       if move != None:
@@ -253,11 +277,12 @@ def generate_legal_moves(player, board):
  # except Exception:
   #  print("No hay movimientos disponibles")
 
-board = [[0,0,0,0],
-         [0,0,0,0],
-         [4,0,3,0],
-         [0,2,1,0]
+board = [[4,0,0,0],
+         [0,3,0,0],
+         [0,0,1,0],
+         [0,2,0,0]
          ]
 
-generate_legal_moves((3,1), board)
-     
+generate_legal_moves(board, "p1")
+
+
